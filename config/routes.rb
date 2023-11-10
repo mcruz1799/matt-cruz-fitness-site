da8b64e1d05893a38c8d7742870eda3c88227c34
Matthew Cruz <matthewcruz@Matthews-Air.lan>
2023-11-09 22:01:57 -0500

    installed Postgres client library to install pg gem; created StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/blog'
+  get 'static_pages/testimonials'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
