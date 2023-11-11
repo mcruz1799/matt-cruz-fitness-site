ffcd1199681ce0144767cef5852cdcf6ce4a0bea
Matthew Cruz <matthewcruz@Matthews-Air.lan>
2023-11-10 19:14:50 -0500

    bug fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'about' to 'static_pages#about'
+  get 'about', to: 'static_pages#about'
   get 'static_pages/blog'
   get 'static_pages/testimonials'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
